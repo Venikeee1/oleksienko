e7f08f2743359b11fb5facb214ab2755188cc22f
Alexander Venik <a.venik@c-i.local>
2018-07-17 18:55:16 +0300

    add 404 page

---
+++ source/handlebars/pages/error-404/data/error-404.js
@@ -0,0 +1,10 @@
+error404: {
+    mainBg: '../source/images/error404/error404-bg.jpg',
+    title: '404',
+    subtitle: 'Page not found',
+    description: [
+        'Oops, this page has been deleted, or never existed.',
+        'Please, click on the menu and select desired section',
+    ],
+    errorText: './source/images/error404/error-text.png'
+}
