58eb3a53a30c531cbe3b2c480f516a32c07e1ae6
Alex <o.vienik@vidaxl.com>
2019-03-04 18:52:57 +0200

    add blog changes

--- source/js/pages/blog/blog.js
+++ source/js/pages/blog/blog.js
@@ -5,12 +5,18 @@ export class Blog {
 
     onInit() {
         window.GLOBAL_OBJECT.header.hideLogo();
+        document.querySelector('body').style.overflow = 'auto';
+        document.querySelector('#barba-wrapper').style.overflow = 'auto';
+        document.querySelector('.barba-container').style.overflow = 'auto';
     }
 
     destroy() {
         window.GLOBAL_OBJECT.header.textAniamtaionAloud = true;
         window.GLOBAL_OBJECT.header.opacityAniamtaionAloud = true;
         window.GLOBAL_OBJECT.header.hideLogoText();
+        document.querySelector('body').style.overflow = 'hidden';
+        document.querySelector('#barba-wrapper').style.overflow = 'hidden';
+        document.querySelector('.barba-container').style.overflow = 'hidden';
     }
 
 
